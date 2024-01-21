bc0944289e33b370394fd7e8702ab76865071866
khalidhub7 <louguikhalid7@gmail.com>
2024-01-21 02:33:33 -0800

    up

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+/**
+ * bubble_sort - sort array of integer
+ * @array: array of int
+ * @size: size of array
+*/
+void bubble_sort(int *array, size_t size)
+{
+	
+	int tmp; /*use for swap*/
+	int i = 0, j = 0; /*used for loop*/
+
+	if (!array || !size)
+	{
+		return;
+	}
+	while (i < size)
+	{
+		for (j = 0; j < size -1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+		i++;
+	}
+}
