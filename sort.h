a8ecebbe854e0f3965b52306c999b8f57626d331
khalidhub7 <louguikhalid7@gmail.com>
2024-01-26 17:26:12 -0800

    up

--- sort.h
+++ sort.h
@@ -84,9 +84,4 @@ void quick_sort_hoare(int *array, size_t size);
 
 
 
-
-void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
-                size_t back);
-void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
-void merge_sort(int *array, size_t size);
 #endif
